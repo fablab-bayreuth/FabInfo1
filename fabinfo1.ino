7caabc358f2ae7824bb6d778d7cf408dba12420c
messlinger <stephan.messlinger@uni-bayreuth.de>
2018-04-06 23:20:22 +0200

    Moved all display stuff to class FabInfoDisplay (FabInfo.h)

--- fabinfo1.ino
+++ fabinfo1.ino
@@ -19,8 +19,8 @@
  (Add http://arduino.esp8266.com/stable/package_esp8266com_index.json to your moard manager path)
 ========================================================================================
  Libraries:
-   Adafruit_GFX
-   Max72xxPanel (XXX Link? XXX)
+   Adafruit_GFX (download via library manager)
+   Max72xxPanel (https://github.com/markruys/arduino-Max72xxPanel)
 ========================================================================================
  Notes for programming the ESP8266:
    There is a cooperative task scheduler running on this device. The user's loop() function
@@ -30,90 +30,34 @@
 ========================================================================================
 */
 
-//========================================================================================
-// User configuration
-//========================================================================================
-
-#define  WIFI_ENABLE   1                  // Turn on Wifi (Station mode) and HTTP Server
-#define  WIFI_SSID  "FabLab_Guest"  // Wifi Accesspoint SSID to connect to
-#define  WIFI_PASS  "Arduino2018"   // Password for Wifi Accesspoint
-#define  HTTP_PORT 80                 // Public port for HTTP server 
-
-#define SERIAL_ENABLE  1                 // Turn on USB-serial connection
-#define SERIAL_BAUDRATE  19200      
-
-const String DEFAULT_TEXT = "Arduino Day 2018 - FabLab Bayreuth";  // Initial display text
-
-//========================================================================================
-// Pin configuration
-//========================================================================================
-
-#define PIN_LED_CS    2   // Chip select pin for led matrix
-#define PIN_LED_DOUT  7   // Data out (SPI MOSI) for led matrix (for reference only, will be used automatically)  
-#define PIN_LED_CLK   5   // Clock out (SPI CLK) for led matrix (for reference only, will be used automatically)
-#define PIN_LDR       A0  // LDR analog voltage
-
-//========================================================================================
-
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Max72xxPanel.h>
+#include "FabInfo.h"
 
-//#include "LedMatrix.h"
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
 //========================================================================================
-// LED matrix display (8 modules a 8x8 LED)
+// User configuration
 //========================================================================================
 
-#define LED_MATRIX_N_HOR   8   // Number of horizontal LED modules
-#define LED_MATRIX_N_VERT  1   // Number of vertical LED modules
+#define  WIFI_ENABLE   0            // Turn on Wifi (Station mode) and HTTP Server
+#define  WIFI_SSID  "FabLab_Guest"  // Wifi Accesspoint SSID to connect to
+#define  WIFI_PASS  "Arduino2018"   // Password for Wifi Accesspoint
+#define  WIFI_WAIT_CONNECT  0       // Wait until connected to Wifi
+#define  HTTP_PORT 80               // Public port for HTTP server 
 
-Max72xxPanel led_matrix( PIN_LED_CS, LED_MATRIX_N_HOR, LED_MATRIX_N_VERT );
+#define SERIAL_ENABLE  1            // Turn on USB-serial interface
+#define SERIAL_BAUDRATE  19200      
 
-String outtext = DEFAULT_TEXT;
-int wait = 40; // In milliseconds
-int spacer = 1;
-int width = 5 + spacer; // The font width is 5 pixels
-
-//========================================================================================
-
-void display_init(void)
-{
-  led_matrix.setIntensity(15); // Use a value between 0 and 15 for brightness
-  for (int i=0; i<LED_MATRIX_N_HOR; i++)  {
-    led_matrix.setRotation(i, 1);
-  }
-}
+#define DEFAULT_TEXT  "Arduino Day 2018 - FabLab Bayreuth"  // Initial display text
 
 //========================================================================================
-
-// TODO: make this non-blocking
-
-void display_scrolltext(String scrt, int clk=40) {
-  for ( int i = 0 ; i < width * scrt.length() + led_matrix.width() - spacer; i++ ) {
-    int letter = i / width;
-    int x = (led_matrix.width() - 1) - i % width;
-    int y = (led_matrix.height() - 8) / 2; // center the text vertically
-    while ( x + width - spacer >= 0 && letter >= 0 ) {
-      if ( letter < scrt.length() ) {
-        led_matrix.drawChar(x, y, scrt[letter], HIGH, LOW, 1);
-      }
-      letter--;
-      x -= width;
-    }
-    led_matrix.write(); // Send bitmap to display
-    delay(clk);
-  }
-}
-
+// FabInfo display
 //========================================================================================
 
-void display_clear(void)
-{
-    led_matrix.fillScreen(0);
-}
+FabInfoDisplay display;
 
 //========================================================================================
 // WIFI
@@ -144,6 +88,38 @@ void wifi_init(void)
   }
 }
 
+
+//========================================================================================
+
+void wifi_task(void)
+{
+  if (WIFI_ENABLE)  {
+    const int wifi_status = WiFi.status();
+    static int old_wifi_status = 0;
+
+    // if Wifi is not connected, try to restart connection
+    if (wifi_status != WL_CONNECTED)   
+      wifi_start();
+
+    // If we get a new Wifi connection: Print the new IP address
+    if (old_wifi_status != WL_CONNECTED  &&  
+           wifi_status == WL_CONNECTED)  {
+      IPAddress myAddr = WiFi.localIP();
+      Serial.println("addr: " + myAddr.toString());
+      display.scroll_text("http://" + myAddr.toString(), 20, 2);
+      display.wait_scroll_text();
+    }
+    old_wifi_status = wifi_status;
+
+    // Note: The low level Wifi tasks are all handled by the underlying
+    //   task scheduler. Just make sure to return from the loop function
+    //   or call delay() periodically to provide enough execution time. 
+  
+    // HTTP server task
+    http_server.handleClient();
+  }
+}
+
 //========================================================================================
 
 void wifi_start()
@@ -152,27 +128,27 @@ void wifi_start()
     ulReconncount++;
   
     // Connect to WiFi network
-    Serial.print("WIFI Connect to: " + (String)WIFI_SSID);
-    display_scrolltext("Connect to: " + (String)WIFI_SSID, 30);
     WiFi.begin( WIFI_SSID, WIFI_PASS );
-  
-    // Wait until connected
-    // TODO: Make this non-blocking!
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print('.');
+
+    if (WIFI_WAIT_CONNECT)  {
+      Serial.print("Wifi connect to: " + String(WIFI_SSID));
+      display.scroll_text("Wifi connect to: " + String(WIFI_SSID));
+
+      int i = 0;
+      while (WiFi.status() != WL_CONNECTED) {   // Wait until connected
+        display.task();
+        delay(10);
+        if (++i % 50 == 0)  Serial.print('.');
+      }
+      Serial.println();
     }
-    Serial.println();
   
-    // Print the IP address
-    IPAddress myAddr = WiFi.localIP();
-    Serial.println("IP address: " + myAddr.toString());
-    display_scrolltext("http://" + myAddr.toString(), 20);
   }
 }
 
 //========================================================================================
-// favicon handler
+// HTTP handlers
+//========================================================================================
 
 #include "favicon.h"
 
@@ -183,10 +159,8 @@ void http_handler_favicon(void)
 }
 
 //========================================================================================
-// HTML page handler
 
 #include "html.h"
-
 
 void http_handler(void)
 {
@@ -198,50 +172,21 @@ void http_handler(void)
 
   if (text.length() > 0)  {
     Serial.print("text: ");
-    Serial.print( http_server.arg("text") );
+    Serial.print( text );
     Serial.println();
-    convert_codepage( text );
-    display_scrolltext( text, 20 );
+    convert_utf8_to_437( text );
+    Serial.println("Via http: " + text);
+    display.scroll_text( text, 20 );
   }
 }
 
 //========================================================================================
-// Helper functions
 
 void http_print_request_info(void)
 {
   Serial.print( http_server.client().remoteIP().toString());
   Serial.print( " " + http_server.uri() );
   Serial.println();
-}
-
-void convert_codepage( String &str )
-{
-  // we have latin1 and want cp437
-  // transpose äöüÄÖÜß
-  str.replace((char)0xE4, (char)0x84);
-  str.replace((char)0xF6, (char)0x94);
-  str.replace((char)0xFC, (char)0x81);
-  str.replace((char)0xC4, (char)0x8E);
-  str.replace((char)0xD6, (char)0x99);
-  str.replace((char)0xDC, (char)0x9A);
-  str.replace((char)0xDF, (char)0xE1);
-}
-
-
-//========================================================================================
-
-void wifi_task(void)
-{
-  if (WIFI_ENABLE)  {
-    // if WIFI is not connected, try to restart connection
-    if (WiFi.status() != WL_CONNECTED)  {
-      wifi_start();
-    }
-  
-    // HTTP server
-    http_server.handleClient();
-  }
 }
 
 
@@ -259,7 +204,7 @@ void serial_init(void)
     Serial.begin(19200);
     //Serial.setDebugOutput(1);
     Serial.println();
-    Serial.println("Hello, World!"); 
+    Serial.println("FabInfo"); 
   }
 }
 
@@ -268,6 +213,7 @@ void serial_init(void)
 void serial_task(void)
 {
   if (SERIAL_ENABLE) {
+    // Receive input characters from serial port and store to internal buffer
     while (Serial.available()) {
       char c = Serial.read();
       if ((c == '\r' || c == '\n') && serial_buf_put > 0)  {  // End command with linebreak
@@ -287,13 +233,15 @@ void serial_task(void)
 
 void serial_eval( const char * input )
 {
-  display_scrolltext( input );
+  Serial.println("Via serial: " + String(input));
+  display.scroll_text( input );
 }
 
 //========================================================================================
+//========================================================================================
 
 void setup() {
-  display_init();
+  display.clear();
   serial_init();
   wifi_init();
 }
@@ -301,8 +249,7 @@ void setup() {
 //========================================================================================
 
 void loop() {
-  display_clear();
-  //  scrolltext(outtext,20);
+  display.task();
   wifi_task();
   serial_task();
 }
